dd10d75bc8dfcc780d7f3e0496dae2741d1f8dbb
Mateusz <mateuszzygadlo00@gmail.com>
2022-04-13 12:40:53 +0200

    Delete dist directory

--- dist/utils/dom/clearSelector.js
+++
@@ -1,3 +0,0 @@
-export function clearSelector({ selector }) {
-    return selector.textContent = '';
-}
--- dist/utils/sleep.js
+++
@@ -1,5 +0,0 @@
-export function sleep(delay) {
-    return new Promise((resolve, reject) => {
-        setTimeout(resolve, delay);
-    });
-}
